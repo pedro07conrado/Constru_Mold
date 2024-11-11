4a11c5e425001c2f2f83e90f3b9ddf413934964e
Pedro Conrado <pedroconrado987@gmail.com>
2024-11-11 16:38:06 -0300

    creates Category and Product models with discount and profit calculation features

--- estoque/models.py
+++ estoque/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=40)
+
+
+    def _str_(self):
+        return self.titulo
+    
+class Produto(models.Model):
+    nome = models.CharField(max_length=40)
+    Categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+    quantidade = models.FloatField()
+    preco_compra = models.BinaryField()
+    preco_venda = models.BinaryField()
+
+    def _str_(self) -> str: 
+        return self.nome
+    
+    def gerar_desconto(self, desconto):
+        return self.preco_venda - ((self.preco_venda * desconto) / 100)
+
+    def lucro(self):
+        lucro = self.preco_venda - self.preco_compra
+        return (lucro * 100) / self.preco_compra
